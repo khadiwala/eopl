1d8d7c0ed8c05276922097859b287465b0ad3909
rkhadiwala <rkhadiwala@cleversafe.com>
2015-06-28 22:38:06 -0500

    -

--- src/119.clj
+++ src/119.clj
@@ -1,1 +1,8 @@
- (ns 119)
+(defn ofree [exp bound]
+  (if (empty? exp)
+    #{}
+    (if (= (head exp) 'lambda)
+      (ofree ((compose rest rest) exp) (conj bound (getvar exp)))
+
+
+(defn getvar [exp] (head ((compose head rest) exp)))
